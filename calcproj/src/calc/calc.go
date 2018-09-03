ed250e4ac793da44fcbac37cc04248dbd16f7835
lovewinder <529129461@qq.com>
2018-09-03 10:43:49 +0800

    "args" setting error

--- calcproj/src/calc/calc.go
+++ calcproj/src/calc/calc.go
@@ -17,14 +17,14 @@ func main(){
 		return
 	}
 	
-	switch args[0]{
+	switch args[1]{
 		case "add":
-			if len(args)!=3{
-				fmt.Println("USAGE:calc add <integerl><integer2>")
+			if len(args)!=4{
+				fmt.Println("USAGE:calc add <integer1><integer2>")
 				return
 			}
-			v1,err1:=strconv.Atoi(args[1])
-			v2,err2:=strconv.Atoi(args[2])
+			v1,err1:=strconv.Atoi(args[2])
+			v2,err2:=strconv.Atoi(args[3])
 			if err1 != nil||err2!=nil{
 				fmt.Println("USAGE:calc add <integer1><integer2>")
 				return
@@ -32,11 +32,11 @@ func main(){
 			ret:=simplemath.Add(v1,v2)
 			fmt.Println("Result: ",ret)
 		case "sqrt":
-			if len(args) != 2{
+			if len(args) != 3{
 				fmt.Println("USAGE: calc sqrt <integer>")
 				return
 			}
-			v,err :=strconv.Atoi(args[1])
+			v,err :=strconv.Atoi(args[2])
 			if err != nil{
 				fmt.Println("USAGE: calc sqrt <integer>")
 				return
